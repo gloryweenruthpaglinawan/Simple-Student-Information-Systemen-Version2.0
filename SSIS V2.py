b6c8f55c19065a84693644677f6bf4588f33ba06
Gloryween Ruth Canencia Paglinawan <79494182+gloryweenruthpaglinawan@users.noreply.github.com>
2021-06-28 01:08:58 +0800

    Update SSIS V2.py

--- SSIS V2.py
+++ SSIS V2.py
@@ -3,36 +3,50 @@ import sqlite3
 from tkinter import ttk
 from tkinter import messagebox
 
-top = Tk()
-top.title("STUDENT INFORMATION SYSTEM")
-top.geometry("1000x400")
-top.config(bg = 'black')
-top.resizable(False, False)
-top.overrideredirect(True)
-top.attributes('-alpha', 0.0)
-
 def main():
     root = Tk()
     root.title("STUDENT INFORMATION SYSTEM")
-    w = 1000
+    w = 1350
     h = 400
-    root.geometry(f'{w}x{h}+{170}+{150}')
+    root.geometry(f'{w}x{h}+{1}+{150}')
     root.config(bg='black')
     root.resizable(False, False)
 
     conn = sqlite3.connect('StudentsList.db')
     c = conn.cursor()
 
-    '''
-    c.execute("""CREATE TABLE studentlist (
+    c.execute("""CREATE TABLE IF NOT EXISTS studentlist (
               ID_NUMBER,
               NAME,
               GENDER,
               YEAR_LEVEL,
-              COURSE,
-              COURSE_CODE                
+              COURSE_CODE
               )""")
-    '''
+
+    c.execute("""CREATE TABLE IF NOT EXISTS courselist (
+              COURSE_CODE,
+              COURSE
+                )""")
+
+    def filled_table():
+        tree.delete(*tree.get_children())
+        conn = sqlite3.connect('StudentsList.db')
+        c = conn.cursor()
+        c.execute("""SELECT ID_NUMBER, NAME, GENDER, YEAR_LEVEL, courselist.COURSE_CODE, courselist.COURSE FROM studentlist
+                      INNER JOIN courselist ON courselist.COURSE_CODE = studentlist.COURSE_CODE;""")
+
+        records = c.fetchall()
+
+        for i in records:
+            tree.insert('', 'end', value=i)
+
+    def cclist():
+        conn = sqlite3.connect("StudentsList.db")
+        c = conn.cursor()
+        c.execute('''SELECT COURSE_CODE FROM courselist''')
+        result = c.fetchall()
+        result = [i[0] for i in result]
+        return result
 
     def register():
         if E1.get() == '':
@@ -43,34 +57,63 @@ def main():
             return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
         elif yearlvl.get() == 'Select Year Level':
             return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
-        elif course.get() == 'Select Course':
-            return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
         elif course_code.get() == 'Select Course Code':
             return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
+        elif course_code.get() not in cclist():
+            messagebox.showinfo("OH NO!", "PLEASE ENTER COURSE NAME")
+            reg_coursecode.insert(0, course_code.get())
+            print(cclist())
 
         conn = sqlite3.connect('StudentsList.db')
         c = conn.cursor()
-        c.execute("INSERT INTO studentlist VALUES(:ID_NUMBER, :NAME, :GENDER,:YEAR_LEVEL,:COURSE,:COURSE_CODE)",
+        c.execute("INSERT INTO studentlist VALUES(:ID_NUMBER, :NAME, :GENDER,:YEAR_LEVEL,:COURSE_CODE)",
                   {
                       'ID_NUMBER': E1.get(),
                       'NAME': E2.get(),
                       'GENDER': gender.get(),
                       'YEAR_LEVEL': yearlvl.get(),
-                      'COURSE': course.get(),
                       'COURSE_CODE': course_code.get()
-
                   })
 
         conn.commit()
-        messagebox.showinfo("REGISTRATION INFORMATION", "YEHEY! YOU JUST GOT REGISTERED!")
+        if course_code.get() in cclist():
+            messagebox.showinfo("REGISTRATION INFORMATION", "YEHEY! YOU JUST GOT REGISTERED!")
         conn.close()
+
+        filled_table()
 
         E1.delete(0, END)
         E2.delete(0, END)
         gender.set("Select Gender")
         yearlvl.set("Select Year Level")
         course_code.set("Select Course Code")
-        course.set("Select Course")
+
+    def course_reg():
+        if reg_coursecode.get() == '':
+            return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
+        elif reg_course.get() == '':
+            return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
+
+        conn = sqlite3.connect('StudentsList.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO courselist VALUES(:COURSE_CODE, :COURSE)",
+                  {
+                      'COURSE_CODE': reg_coursecode.get(),
+                      'COURSE': reg_course.get()
+                  })
+
+        conn.commit()
+        messagebox.showinfo("COURSE REGISTRATION INFO", "YEHEY! COURSE REGISTERED!")
+        conn.close()
+
+        filled_table()
+
+        course_code["values"] = cclist()
+        root_coursecode['values'] = cclist()
+
+        reg_coursecode.delete(0, END)
+        reg_course.delete(0, END)
 
     def select():
         conn = sqlite3.connect('StudentsList.db')
@@ -79,7 +122,6 @@ def main():
         E4.delete(0, END)
         root_gender.delete(0, END)
         root_yearlvl.delete(0, END)
-        root_course.delete(0, END)
         root_coursecode.delete(0, END)
 
         selected = tree.focus()
@@ -89,8 +131,7 @@ def main():
         E4.insert(0, values[1])
         root_gender.insert(0, values[2])
         root_yearlvl.insert(0, values[3])
-        root_course.insert(0, values[4])
-        root_coursecode.insert(0, values[5])
+        root_coursecode.insert(0, values[4])
 
         conn.commit()
         conn.close()
@@ -104,8 +145,6 @@ def main():
             return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
         elif root_yearlvl.get() == 'Select Year Level':
             return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
-        elif root_course.get() == 'Select Course':
-            return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
         elif root_coursecode.get() == 'Select Course Code':
             return messagebox.showwarning("WARNING!", "PLEASE TRY TO COMPLETE THE INPUT")
 
@@ -116,47 +155,36 @@ def main():
         data2 = E4.get()
         data3 = root_gender.get()
         data4 = root_yearlvl.get()
-        data5 = root_course.get()
-        data6 = root_coursecode.get()
+        data5 = root_coursecode.get()
 
         selected = tree.selection()
-        tree.item(selected, values=(data1, data2, data3, data4, data5, data6))
+        tree.item(selected, values=(data1, data2, data3, data4, data5))
         c.execute(
-            "UPDATE studentlist set  ID_NUMBER=?, NAME=?, GENDER=?, YEAR_LEVEL=?, COURSE=?, COURSE_CODE=?  WHERE ID_NUMBER=? ",
-            (data1, data2, data3, data4, data5, data6, data1))
+            "UPDATE studentlist set  ID_NUMBER=?, NAME=?, GENDER=?, YEAR_LEVEL=?, COURSE_CODE=?  WHERE ID_NUMBER=? ",
+            (data1, data2, data3, data4, data5, data1))
 
         conn.commit()
         conn.close()
-
-    def refresh():
-        root.destroy()
-        root2 = Tk()
-        root2.title("STUDENT INFORMATION SYSTEM")
-        root2.geometry("1000x400")
-        root2.config(bg='black')
-        root2.resizable(False, False)
-        root2.overrideredirect(True)
-        root2.attributes('-alpha', 0.0)
-
-        def des():
-            root2.destroy()
-
-        root2.after(10, lambda: (des(), main()))
-
-        root2.mainloop()
+        filled_table()
 
     def delete():
-        if messagebox.askyesno("DELETE CONFIRMATION", "ARE YOU SURE?") == False:
-            return
+        selected_item = tree.focus()
+
+        if selected_item == "":
+            messagebox.showwarning("ERROR!", "PLEASE SELECT INFORMATION TO DELETE")
+
         else:
-            messagebox.showinfo("DELETE CONFIRMATION", "DATA SUCCESSFULLY DELETED")
-            conn = sqlite3.connect("StudentsList.db")
-            c = conn.cursor()
-            for selected_item in tree.selection():
-                c.execute("DELETE FROM studentlist WHERE ID_number=?", (tree.set(selected_item, '#1'),))
-                conn.commit()
-                tree.delete(selected_item)
-            conn.close()
+            if messagebox.askyesno("DELETE CONFIRMATION", "ARE YOU SURE?") == False:
+                return
+            else:
+                messagebox.showinfo("DELETE CONFIRMATION", "DATA SUCCESSFULLY DELETED")
+                conn = sqlite3.connect("StudentsList.db")
+                c = conn.cursor()
+                for selected_item in tree.selection():
+                    c.execute("DELETE FROM studentlist WHERE ID_NUMBER=?", (tree.set(selected_item, '#1'),))
+                    conn.commit()
+                    tree.delete(selected_item)
+                conn.close()
 
     def search():
         root1 = Tk()
@@ -168,7 +196,8 @@ def main():
         conn = sqlite3.connect('StudentsList.db')
         c = conn.cursor()
 
-        c.execute("SELECT * FROM studentlist WHERE ID_NUMBER=? ", (E3.get(),))
+        c.execute("""SELECT ID_NUMBER, NAME, GENDER, YEAR_LEVEL, courselist.COURSE_CODE, courselist.COURSE FROM studentlist
+                       INNER JOIN courselist ON courselist.COURSE_CODE = studentlist.COURSE_CODE WHERE ID_NUMBER=?""", (E3.get(),))
         records = c.fetchall()
 
         frm = Frame(root1)
@@ -184,9 +213,9 @@ def main():
         tree.column("3", minwidth=0, width=150)
         tree.heading(4, text="YEAR LEVEL", anchor=CENTER)
         tree.column("4", minwidth=0, width=150)
-        tree.heading(5, text="COURSE", anchor=CENTER)
-        tree.heading(6, text="COURSE CODE", anchor=CENTER)
-        tree.column("6", minwidth=0, width=150)
+        tree.heading(5, text="COURSE CODE", anchor=CENTER)
+        tree.column("5", minwidth=0, width=150)
+        tree.heading(6, text="COURSE", anchor=CENTER)
 
         for i in records:
             tree.insert('', 'end', value=i)
@@ -197,12 +226,16 @@ def main():
 
         root1.mainloop()
 
-    Reg = LabelFrame(root, text="Registration Form", width=500, height=200, bg="black", fg="white",
+    Reg = LabelFrame(root, text="Registration Form", width=465, height=200, bg="black", fg="white",
                      font=("Lucida Console", 15, "bold"))
     Reg.place(x=500, y=0)
     Upd_Del = LabelFrame(root, text="Update and Delete Form", width=500, height=200, bg="black", fg="white",
                          font=("Lucida Console", 15, "bold"))
     Upd_Del.place(x=0, y=0)
+    Course = LabelFrame(root, text="Course Registration Form", width=385, height=200, bg="black", fg="white",
+                     font=("Lucida Console", 15, "bold"))
+    Course.place(x=965, y=0)
+
     L1 = Label(root, text="ID NUMBER:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
     L1.place(x=510, y=35)
     L2 = Label(root, text="NAME:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
@@ -211,10 +244,8 @@ def main():
     L3.place(x=510, y=85)
     L4 = Label(root, text="YEAR LEVEL:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
     L4.place(x=510, y=110)
-    L5 = Label(root, text="COURSE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
+    L5 = Label(root, text="COURSE CODE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
     L5.place(x=510, y=135)
-    L6 = Label(root, text="COURSE CODE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
-    L6.place(x=510, y=160)
 
     E1 = Entry(root, bd=2, width=27, font=("Lucida Console", 10))
     E1.place(x=650, y=35)
@@ -231,16 +262,10 @@ def main():
     yearlvl['values'] = ("1st Year", "2nd Year", "3rd Year", "4th Year", "5th Year")
     yearlvl.place(x=650, y=110)
 
-    course = ttk.Combobox(root, width=25, font=("Lucida Console", 10))
-    course.set("Select Course")
-    course['values'] = (
-    "BS in Computer Science", "BS in Computer Application", "BS in Information Technology", "BS in Information System")
-    course.place(x=650, y=135)
-
     course_code = ttk.Combobox(root, width=25, font=("Lucida Console", 10))
     course_code.set("Select Course Code")
-    course_code['values'] = ("BSCS", "BSCA", "BSIT", "BSIS")
-    course_code.place(x=650, y=160)
+    course_code['values'] = cclist()
+    course_code.place(x=650, y=135)
 
     Sel = Button(root, text="SELECT", font=("Lucida Console", 9, "bold"), command=select)
     Sel.place(x=300, y=15)
@@ -251,16 +276,17 @@ def main():
     Sea = Button(root, text="SEARCH", font=("Lucida Console", 9, "bold"), command=search)
     Sea.place(x=430, y=40)
     Reg = Button(root, text="REGISTER", font=("Lucida Console", 9, "bold"), command=register)
-    Reg.place(x=900, y=170)
-    Ref = Button(root, text="REFRESH", font=("Lucida Console", 9, "bold"), command=refresh)
-    Ref.place(x=420, y=170)
+    Reg.place(x=865, y=170)
+    Course_Reg = Button(root, text="REGISTER", font=("Lucida Console", 9, "bold"), command=course_reg)
+    Course_Reg.place(x=1250, y=170)
 
     conn = sqlite3.connect('StudentsList.db')
     c = conn.cursor()
 
-    c.execute("SELECT * FROM studentlist")
+    c.execute("""SELECT ID_NUMBER, NAME, GENDER, YEAR_LEVEL, courselist.COURSE_CODE, courselist.COURSE FROM studentlist
+              INNER JOIN courselist ON courselist.COURSE_CODE = studentlist.COURSE_CODE;""")
+
     records = c.fetchall()
-    total = c.rowcount
 
     frm = Frame(root)
     frm.pack(side=LEFT, padx=5, pady=(200, 0))
@@ -269,15 +295,17 @@ def main():
     tree.pack()
 
     tree.heading(1, text="ID NUMBER", anchor=CENTER)
-    tree.column("1", minwidth=0, width=150)
+    tree.column("1", minwidth=0, width=200)
     tree.heading(2, text="NAME", anchor=CENTER)
+    tree.column("2", minwidth=0, width=270)
     tree.heading(3, text="GENDER", anchor=CENTER)
-    tree.column("3", minwidth=0, width=150)
+    tree.column("3", minwidth=0, width=200)
     tree.heading(4, text="YEAR LEVEL", anchor=CENTER)
-    tree.column("4", minwidth=0, width=150)
-    tree.heading(5, text="COURSE", anchor=CENTER)
-    tree.heading(6, text="COURSE CODE", anchor=CENTER)
-    tree.column("6", minwidth=0, width=150)
+    tree.column("4", minwidth=0, width=200)
+    tree.heading(5, text="COURSE CODE", anchor=CENTER)
+    tree.column("5", minwidth=0, width=200)
+    tree.heading(6, text="COURSE", anchor=CENTER)
+    tree.column("6", minwidth=0, width=270)
 
     for i in records:
         tree.insert('', 'end', value=i)
@@ -297,16 +325,10 @@ def main():
     root_yearlvl['values'] = ("1st Year", "2nd Year", "3rd Year", "4th Year", "5th Year")
     root_yearlvl.place(x=130, y=125)
 
-    root_course = ttk.Combobox(root, width=25, font=("Lucida Console", 10))
-    root_course.set("Select Course")
-    root_course['values'] = (
-    "BS in Computer Science", "BS in Computer Application", "BS in Information Technology", "BS in Information System")
-    root_course.place(x=130, y=150)
-
     root_coursecode = ttk.Combobox(root, width=25, font=("Lucida Console", 10))
     root_coursecode.set("Select Course Code")
-    root_coursecode['values'] = ("BSCS", "BSCA", "BSIT", "BSIS")
-    root_coursecode.place(x=130, y=175)
+    root_coursecode['values'] = cclist()
+    root_coursecode.place(x=130, y=150)
 
     L7 = Label(root, text="ID NUMBER TO FIND:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
     L7.place(x=5, y=38)
@@ -316,19 +338,22 @@ def main():
     L9.place(x=5, y=100)
     L10 = Label(root, text="YEAR LEVEL:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
     L10.place(x=5, y=125)
-    L11 = Label(root, text="COURSE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
+    L11 = Label(root, text="COURSE CODE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
     L11.place(x=5, y=150)
+
     L12 = Label(root, text="COURSE CODE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
-    L12.place(x=5, y=175)
+    L12.place(x=970, y=80)
+    L13 = Label(root, text="COURSE:", bg="black", fg="white", font=("Lucida Console", 11, "bold"))
+    L13.place(x=970, y=105)
+
+    reg_coursecode = Entry(root, bd=2, width=27, font=("Lucida Console", 10))
+    reg_coursecode.place(x=1100, y=80)
+    reg_course = Entry(root, bd=2, width=27, font=("Lucida Console", 10))
+    reg_course.place(x=1100, y=105)
 
     conn.commit()
     conn.close()
 
     root.mainloop()
 
-def des():
-    top.destroy()
-
-top.after(100, lambda :(des(), main()))
-
-top.mainloop()
+main()
